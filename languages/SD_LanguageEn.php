90349a7a48f45ecd714184190ab83cc1aba8a701
Yaron Koren <yaron@users.mediawiki.org>
2008-01-08 21:33:04 +0000

    Re-added namespace values

--- languages/SD_LanguageEn.php
+++ languages/SD_LanguageEn.php
@@ -7,11 +7,14 @@ class SD_LanguageEn extends SD_Language {
 
 // no need to define properties or namespaces - the English-language
 // values are already specified in the default aliases
+// (namespaces are still defined because namespace aliases don't work yet)
 
 /* private */ var $m_SpecialProperties = array(
 );
 
 var $m_Namespaces = array(
+	SD_NS_FILTER		=> 'Filter', 	 
+	SD_NS_FILTER_TALK	=> 'Filter_talk'
 );
 
 }
