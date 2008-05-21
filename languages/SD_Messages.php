7f433b61bc3f3ab8eb02fb5751ea5913a8501ba7
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-05-21 22:56:07 +0000

    Localisation updates. Update credits.

--- languages/SD_Messages.php
+++ languages/SD_Messages.php
@@ -837,7 +837,7 @@ $messages['pt'] = array(
 );
 
 /** Russian (Русский)
- * @author .:Ajvol:.
+ * @author Александр Сигачёв
  */
 $messages['ru'] = array(
 	'browsedata'                            => 'Обзор данных',
