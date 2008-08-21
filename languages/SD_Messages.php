24a772c07aa17bbe9a563e147098d1ac74f77812
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-21 15:58:59 +0000

    Localisation updates for extensions messages from Betawiki (2008-08-21 17:36 CEST)

--- languages/SD_Messages.php
+++ languages/SD_Messages.php
@@ -392,6 +392,7 @@ $messages['fi'] = array(
  * @author Urhixidur
  */
 $messages['fr'] = array(
+	'specialpages-group-sd_group' => 'Exercice de sémantique',
 	'browsedata' => 'Chercher les données',
 	'sd_browsedata_choosecategory' => 'Choisir une catégorie',
 	'sd_browsedata_viewcategory' => 'Voir la catégorie',
@@ -761,6 +762,7 @@ $messages['nds'] = array(
  * @author Siebrand
  */
 $messages['nl'] = array(
+	'specialpages-group-sd_group' => 'Semantic Drilldown',
 	'browsedata' => 'Gegevens bekijken',
 	'sd_browsedata_choosecategory' => 'Kies een categorie',
 	'sd_browsedata_viewcategory' => 'categorie bekijken',
@@ -866,6 +868,7 @@ $messages['nso'] = array(
  * @author Cedric31
  */
 $messages['oc'] = array(
+	'specialpages-group-sd_group' => 'Exercici de semantica',
 	'browsedata' => 'Cercar las donadas',
 	'sd_browsedata_choosecategory' => 'Causir una categoria',
 	'sd_browsedata_viewcategory' => 'Veire la categoria',
@@ -1058,6 +1061,7 @@ $messages['ru'] = array(
  * @author Helix84
  */
 $messages['sk'] = array(
+	'specialpages-group-sd_group' => 'Sémantické operácie',
 	'browsedata' => 'Prehliadať údaje',
 	'sd_browsedata_choosecategory' => 'Vyberte kategóriu',
 	'sd_browsedata_viewcategory' => 'zobraziť kategóriu',
