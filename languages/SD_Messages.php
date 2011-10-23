76e24109383660abf8c2f43355148f6e5cff2d73
Raimond Spekking <raymond@users.mediawiki.org>
2011-10-23 19:48:36 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- languages/SD_Messages.php
+++ languages/SD_Messages.php
@@ -486,6 +486,7 @@ $messages['br'] = array(
 	'sd_createfilter_requirefilter' => 'Goulenn ma vo diuzet ur sil all a-raok na zeufe hemañ war wel :',
 	'sd_createfilter_label' => 'Tiketenn evit ar sil-mañ (diret) :',
 	'sd_blank_error' => "ne c'hall ket bezañ goullo",
+	'sd-pageschemas-filter' => 'Sil',
 	'sd-pageschemas-values' => 'Talvoudoù',
 	'sd_filter_coversproperty' => "Ar sil-mañ a ra war-dro ar perc'henniezh $1.",
 	'sd_filter_getsvaluesfromcategory' => 'E dalvoudoù en deus eus ar rummad $1.',
