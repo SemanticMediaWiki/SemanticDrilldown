e9388e51ea5d93787d8ea52423627aa1c539da30
yaron <yaron57@gmail.com>
2013-06-11 18:41:36 +0400

    New version: 1.2.5
    Change-Id: I39fbbb74844dacb70b0b0cbeb681cc13db0b78cc

--- SemanticDrilldown.php
+++ SemanticDrilldown.php
@@ -13,7 +13,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SD_VERSION', '1.2.4' );
+define( 'SD_VERSION', '1.2.5' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'specialpage'][] = array(
 	'path'        => __FILE__,
