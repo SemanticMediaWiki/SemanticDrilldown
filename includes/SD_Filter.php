f76cf92012c05332de160061dfcc016e45d77e13
Joel Natividad <joel.natividad@ontodia.com>
2014-07-13 22:33:54 -0400

    Added long-string handling
    When long strings in smw_di_blob are displayed, display readable value,
    not the value with the hash suffix.
    
    Change-Id: I413a75ebaf3c28cf935aa0afebc25ba241e3473a

--- includes/SD_Filter.php
+++ includes/SD_Filter.php
@@ -175,7 +175,7 @@ class SDFilter {
 				$this->db_value_field = 'o_serialized';
 			} else { // string, text, code
 				$this->db_table_name = 'smw_di_blob';
-				$this->db_value_field = 'o_hash';
+				$this->db_value_field = '(CASE WHEN octet_length(o_blob) > 1 THEN CONVERT(o_blob using utf8) ELSE o_hash END)';
 			}
 		} else {
 			// Things used to be so simple...
