a6695bcf11d96ef0da7966939a5c269a1e147796
yvarravy <yvar@archixl.nl>
2023-12-20 16:30:09 +0100

    overwrite getQueryInfo instead of getSQL

--- includes/Specials/BrowseData/QueryPage.php
+++ includes/Specials/BrowseData/QueryPage.php
@@ -10,6 +10,9 @@ use SD\Parameters\Parameters;
 use SD\Sql\SqlProvider;
 use SMWOutputs;
 use Title;
+use SD\Utils;
+use Wikimedia\Rdbms\Subquery;
+use SD\Sql\PropertyTypeDbInfo;
 
 class QueryPage extends \QueryPage {
 
@@ -111,6 +114,156 @@ class QueryPage extends \QueryPage {
 		}
 
 		return ( $this->processTemplate ) ( 'QueryPageHeader', $vm );
+	}
+
+	public function getQueryInfo() {
+		if ( !$this->query ) {
+			return 'select null as sortkey where 0 = 1';
+		}
+		$dbr = \MediaWiki\MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection( DB_REPLICA );
+		$smwIDs = $dbr->tableName( Utils::getIDsTableName() );
+		$smwCategoryInstances = $dbr->tableName( Utils::getCategoryInstancesTableName() );
+		$cat_ns = NS_CATEGORY;
+		$prop_ns = SMW_NS_PROPERTY;
+
+		$query = [
+			'fields' => [
+				'title' => 'ids.smw_title',
+				'value' => 'ids.smw_title',
+				't' => 'ids.smw_title',
+				'namespace' => 'ids.smw_namespace',
+				'ns' => 'ids.smw_namespace',
+				'id' => 'ids.smw_id',
+				'iw' => 'ids.smw_iw',
+				'sortkey' => 'ids.smw_sortkey',
+			],
+			'tables' => [
+				'ids' => $smwIDs,
+				'insts' => $smwCategoryInstances
+			],
+			'join_conds' => [
+				'insts' => [
+					'JOIN',
+					[
+						'ids.smw_id = insts.s_id' ,
+						'ids.smw_namespace != ' . $cat_ns
+					]
+				]
+			],
+			'conds' => []
+		];
+		$applied_filters = $this->query->appliedFilters();
+		foreach ( $applied_filters as $i => $af ) {
+			// if any of this filter's values is 'none',
+			// include another table to get this information
+			$includes_none = false;
+			foreach ( $af->values as $fv ) {
+				if ( $fv->text === '_none' || $fv->text === ' none' ) {
+					$includes_none = true;
+					break;
+				}
+			}
+			if ( $includes_none ) {
+				$property_table_name = $dbr->tableName(
+					PropertyTypeDbInfo::tableName( $af->filter->propertyType() ) );
+				if ( $af->filter->propertyType() === 'page' ) {
+					$property_table_nickname = "nr$i";
+					$property_field = 'p_id';
+				} else {
+					$property_table_nickname = "na$i";
+					$property_field = 'p_id';
+				}
+				$property_value = str_replace( ' ', '_', $af->filter->property() );
+				$property_value = str_replace( "'", "\'", $property_value );
+				// The sub-query that returns an SMW ID contains
+				// a "SELECT MIN", even though by definition it
+				// doesn't need to, because of occasional bugs
+				// in SMW where the same page gets two
+				// different SMW IDs.
+				$query[ 'tables' ][ $property_table_nickname ] = new Subquery( "SELECT s_id FROM $property_table_name WHERE $property_field = (SELECT MIN(smw_id) FROM $smwIDs WHERE smw_title = '$property_value' AND smw_namespace = $prop_ns)" );
+				$query[ 'join_conds' ][ $property_table_nickname ] = [
+					"LEFT OUTER JOIN",
+					[
+						"ids.smw_id = $property_table_nickname.s_id"
+					]
+				];
+			}
+		}
+		foreach( $applied_filters as $i => $af ) {
+			$property_table_name = $dbr->tableName(
+				PropertyTypeDbInfo::tableName( $af->filter->propertyType() ) );
+			if ( $af->filter->propertyType() === 'page' ) {
+				$sql = "";
+				if ( $includes_none ) {
+					$sql .= "LEFT OUTER ";
+				}
+				$sql .= "JOIN";
+				$query[ 'tables' ][ 'r' . $i ] = $property_table_name;
+				$query[ 'join_conds' ][ 'r' . $i ] = [
+					$sql,
+					[
+						"ids.smw_id = r$i.s_id"
+					]
+				];
+				$sql = "";
+				if ( $includes_none ) {
+					$sql .= "LEFT OUTER ";
+				}
+				$sql .= "JOIN";
+				$query[ 'tables' ][ 'o_ids' . $i ] = $smwIDs;
+				$query[ 'join_conds' ][ 'o_ids' . $i ] = [
+					$sql,
+					[
+						"r$i.s_id = o_ids$i.smw_id"
+					]
+				];
+			} else {
+				$query[ 'tables' ][ 'a' . $i ] = $property_table_name;
+				$query[ 'join_conds' ][ 'a' . $i ] = [
+					'JOIN',
+					[
+						"ids.smw_id = a$i.s_id"
+					]
+				];
+			}
+		}
+		$category = $this->query->category();
+		$subcategory = $this->query->subcategory();
+		$actual_cat = str_replace( ' ', '_', ($subcategory) ? $subcategory : $category );
+		$actual_cat = str_replace( "'", "\'", $actual_cat );
+
+		$sql = "(SELECT smw_id FROM $smwIDs cat_ids WHERE smw_namespace = $cat_ns AND (smw_title = '$actual_cat'";
+		foreach ( $this->query->allSubcategories() as $subcat ) {
+			$subcat = str_replace( "'", "\'", $subcat );
+			$sql .= " OR smw_title = '{$subcat}'";
+		}
+		$sql .= ")) ";
+		$query['conds']['insts.o_id'] = new Subquery( $sql );
+		foreach ( $applied_filters as $i => $af ) {
+			$property_value = $af->filter->escapedProperty();
+			$value_field = PropertyTypeDbInfo::valueField( $af->filter->propertyType() );//
+			if ( $af->filter->propertyType() === 'page' ) {
+				$property_field = "r$i.p_id";
+				$sql = "($property_field = (SELECT MIN(smw_id) FROM $smwIDs WHERE smw_title = '$property_value' AND smw_namespace = $prop_ns)";
+				if ( !$includes_none ) {
+					$sql .= " OR $property_field IS NULL";
+				}
+				$sql .= ")";
+				$value_field = "o_ids$i.smw_title";
+				$query[ 'conds' ][ $property_field ] = new Subquery( $sql );
+			} else {
+				$property_field = "a$i.p_id";
+				$sql = "(SELECT MIN(smw_id) FROM $smwIDs WHERE smw_title = '$property_value' AND smw_namespace = $prop_ns) AND ";
+				if ( strncmp( $value_field, '(IF(o_blob IS NULL', 18 ) === 0 ) {
+					$value_field = str_replace( 'o_', "a$i.o_", $value_field );
+				} else {
+					$value_field = "a$i.$value_field";
+				}
+				$query[ 'conds' ][ $property_field ] = new Subquery( $sql );
+			}
+		}
+
+		return $query;
 	}
 
 	protected function getSQL(): ?string {
