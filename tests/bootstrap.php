e2c15bdafc2fd855c6bc5b5a2f636d182a3bcd69
paladox <paladox@users.noreply.github.com>
2024-12-18 17:35:28 +0000

    Create bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+if ( PHP_SAPI !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
+error_reporting( E_ALL | E_STRICT );
+date_default_timezone_set( 'UTC' );
+ini_set( 'display_errors', 1 );
+
+global $IP;
+
+if ( !is_readable( $autoloaderClassPath = $IP . '/extensions/SemanticMediaWiki/tests/autoloader.php' ) ) {
+	die( "\nThe Semantic MediaWiki test autoloader is not available\n" );
+}
+
+if ( ExtensionRegistry::getInstance()->isLoaded( 'SemanticDrilldown' ) ) {
+	die( "\nSemantic Drilldown is not available, please check your Composer or LocalSettings.\n" );
+}
+
+$autoloader = require $autoloaderClassPath;
+unset( $autoloader );
