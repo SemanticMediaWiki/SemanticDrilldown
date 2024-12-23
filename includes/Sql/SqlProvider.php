3e9c93f1ed7210e7a1f4a997ac443a75156ea9d7
paladox <paladox@users.noreply.github.com>
2024-12-23 22:38:00 +0000

    Update SqlProvider.php

--- includes/Sql/SqlProvider.php
+++ includes/Sql/SqlProvider.php
@@ -49,8 +49,8 @@ class SqlProvider {
 	 */
 	public static function getSQLFromClauseForCategory( $subcategory, $child_subcategories ) {
 		$dbr = MediaWikiServices::getInstance()
- 				->getDBLoadBalancer()
- 				->getMaintenanceConnectionRef( DB_REPLICA );
+			->getDBLoadBalancer()
+			->getMaintenanceConnectionRef( DB_REPLICA );
 		$smwIDs = $dbr->tableName( Utils::getIDsTableName() );
 		$smwCategoryInstances = $dbr->tableName( Utils::getCategoryInstancesTableName() );
 		$ns_cat = NS_CATEGORY;
@@ -82,8 +82,8 @@ class SqlProvider {
 	 */
 	public static function getSQLFromClause( string $category, string $subcategory, array $subcategories, array $applied_filters ) {
 		$dbr = MediaWikiServices::getInstance()
- 				->getDBLoadBalancer()
- 				->getMaintenanceConnectionRef( DB_REPLICA );
+			->getDBLoadBalancer()
+			->getMaintenanceConnectionRef( DB_REPLICA );
 		$smwIDs = $dbr->tableName( Utils::getIDsTableName() );
 		$smwCategoryInstances = $dbr->tableName( Utils::getCategoryInstancesTableName() );
 		$cat_ns = NS_CATEGORY;
